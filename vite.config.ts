2b36559fedd4dfade73461072312a3b8bbed76f7
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2024-02-05 11:01:10 +0800

    Upgrade Deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,5 @@ import { defineConfig } from "vite"
 import electron from "vite-plugin-electron"
 
 export default defineConfig({
-    plugins: [
-        react(),
-        electron({
-            vite: {
-                build: {
-                    outDir: "dist-electron",
-                    lib: {
-                        entry: ["electron/main.ts"],
-                        fileName: (f, n) => `${n}.${f}`,
-                        formats: ["cjs"]
-                    }
-                }
-            }
-        })
-    ]
+    plugins: [react(), electron({ entry: ["electron/main.ts"] })]
 })

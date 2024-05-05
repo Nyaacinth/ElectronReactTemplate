aca1f7aaa3bcc46c8878547c4aa566bac8ea21e1
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2024-05-05 12:28:47 +0800

    Fix outdated

--- app/views/Splash.tsx
+++ app/views/Splash.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react"
+import type { FunctionComponent } from "react"
 
 export const Splash: FunctionComponent = () => {
     return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import react from "@vitejs/plugin-react-swc"
+import path from "node:path"
 import { defineConfig } from "vite"
-import electron from "vite-plugin-electron"
+import electron from "vite-plugin-electron/simple"
 
 export default defineConfig({
-    plugins: [react(), electron({ entry: ["electron/main.ts"] })]
+    plugins: [
+        react(),
+        electron({
+            main: {
+                entry: "electron/main.ts"
+            },
+            preload: {
+                input: path.join(__dirname, "electron/preload.ts")
+            },
+            renderer: process.env.NODE_ENV === "test" ? undefined : {}
+        })
+    ]
 })
